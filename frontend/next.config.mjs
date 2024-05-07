00491a4d1646b781adcfe7bee326ade5dcae8ad4
devcowsar <kawsarcse836@gmail.com>
2024-05-08 00:24:14 +0600

    fix: gitnext

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,4 +3,8 @@ const nextConfig = {
   output: 'standalone'
 };
 
+module.exports = {
+  cacheHandler: require.resolve('./cache-handler.js'),
+  cacheMaxMemorySize: 0, // disable default in-memory caching
+}
 export default nextConfig;
